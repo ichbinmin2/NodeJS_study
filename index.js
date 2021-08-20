d4f7453099018f5d40bb8d41f892c9ee52442f38
JiyeonMin <ichbinmin2@gmail.com>
2021-08-20 15:59:52 +0900

    ADD : node mon 추가

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://teta:tetaMongodb@tetaproject1.slyfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-})
-.then(()=> console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+const bodyParser = require(`body-parser`);
+const { User } = require("./models/User");
 
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// application/json
+app.use(express.json());
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://teta:tetaMongodb@tetaproject1.slyfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// 회원가입용 라우트
+app.post("/register", (req, res) => {
+  // 클라이언트에서 보내주는 회원가입 정보들을 가져와서
+  // 데이터 베이스에 넣어주는 역할을 해준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
